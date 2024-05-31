0b6cd91a3118222c0f5def6aa16ec611fe166510
小城 <kojolumi@gamil.com>
2024-05-31 13:21:37 +0900

    test

---
+++ footer.php
@@ -0,0 +1,38 @@
+</main>
+
+
+    <!-- footer -->
+    <footer>
+        <div class="bg-[#F2F2F2] w-[100%] pt-[180px] pb-[30px] px-20 text-[#30554C]">
+            <h1 class="text-xl pb-4">株式会社テストサイト
+            </h1>
+            <table>
+                <tr>
+                <td>上場証券取引所</td>
+                <td>：〇〇〇〇 プライム市場</td>
+                </tr>
+                <tr>
+                <td>創業</td>
+                <td>：1922年</td>
+                </tr>
+                <tr>
+                <td>資本金</td>
+                <td>：〇〇〇〇円</td>
+                </tr>
+                <tr>
+                <td>本社所在地</td>
+                <td>：〒xxx-xxxx 大阪府大阪市〇〇〇〇 〇〇ビルディング</td>
+                </tr>
+            </table>
+        </div>
+        <p class="bg-[#30554c] h-[53px] text-white text-center content-center">
+            &copy;Kojo Test
+        </p>
+    </footer>
+
+
+    <!-- script -->
+    <script src="./js/script.js"></script>
+    <?php wp_footer(); ?>
+</body>
+</html>
